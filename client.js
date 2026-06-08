7c8886674aefc5bdac1a79c480f2767479acffed
jerrythomas-85 <jerry.thomas@auratus.pt>
2026-06-08 11:54:33 +0100

    Update client.js

--- client.js
+++ client.js
@@ -1,1 +1,15 @@
-
+TrelloPowerUp.initialize({
+  'card-buttons': function(t, options) {
+    return [{
+      icon: 'https://cdn-icons-png.flaticon.com/512/561/561127.png',
+      text: 'CRM Auratus',
+      callback: function(t) {
+        return t.popup({
+          title: 'CRM Auratus',
+          url: './sidebar.html',
+          height: 600
+        });
+      }
+    }];
+  }
+});
